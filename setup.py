e12fb441fad7c1cc4178ff1debbbca761b386ba0
Daniel Waardal <daniel@massforstroel.se>
2013-07-02 17:22:48 +0200

    project specific setup created

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+    name='gramme',
+    version='0.1.0',
+    url='http://weighted-random.github.com/wr/',
+    license='BSD',
+    author='Daniel Waardal',
+    author_email='waawal@boom.ws',
+    description='Elegant UDP messaging',
+    long_description=long_description,
+    py_modules=['gramme'],
+    zip_safe=True,
+    platforms='any',
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        #'Development Status :: 1 - Planning',
+        #'Development Status :: 2 - Pre-Alpha',
+        #'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        #'Development Status :: 5 - Production/Stable',
+        #'Development Status :: 6 - Mature',
+        #'Development Status :: 7 - Inactive',
+        #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.5',
+        #'Programming Language :: Python :: 2.6',
+        #'Programming Language :: Python :: 2.7',
+        #'Programming Language :: Python :: 3',
+        #'Programming Language :: Python :: 3.0',
+        #'Programming Language :: Python :: 3.1',
+        #'Programming Language :: Python :: 3.2',
+        #'Programming Language :: Python :: 3.3',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: System :: Systems Administration',
+
+    ]
+)
